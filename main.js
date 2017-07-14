095bbfea3cccb875695d83dcfea237f906d5dbec
James Spargo <solacedforever@gmail.com>
2017-07-14 19:56:40 -0400

    changes 2

--- main.js
+++ main.js
@@ -1,12 +1,4 @@
-// // Since the `data.js` file is loaded in your `index.html` before this one,
-// // you have access to the `data` variable that contains the Etsy Store data.
-// // Open your `index.html` page in your browser and you can verify that the following
-// // is properly working. The `data` variable is an array with 25 items in it
-// console.log(data);
-//
-//
-//
-// 1: Show me how to calculate the average price of all items.
+
 function question1() {
   // Answer:
   let sum = 0;
@@ -31,17 +23,17 @@ function question2() {
   console.log(moo);
 }
 
-
-
-let cost = [];
-for (var i = 0; data.length; i++) {
-  let value = data[i].price;
-  if (value <= 14 && value <= 18) {
-    cost.push(value);
-  }
-  console.log(cost);
-}
-
+//
+//
+// let cost = [];
+// for (var i = 0; i > data.length; i++) {
+//   let value = data[i].price;
+//   if (value <= 14 && value <= 18) {
+//     cost.push(value);
+//   }
+//   console.log(cost);
+// }
+//
 
 
 // 3: Which item has a "GBP" currency code? Display it's name and price.
@@ -76,14 +68,14 @@ function question4() {
 function question5() {
   // Answer:
   data.forEach(function(item) {
-    if (items.materials.length > 8) {
+    if (item.materials.length > 8) {
       console.log(item.title, item.quantity,
         item.materials.forEach(function(material) {
           console.log("The items are ", material)
         }));
     }
   });
-
+}
 
   // 6: How many items were made by their sellers?
   // Answer:
@@ -91,9 +83,9 @@ function question5() {
     // Answer:
     let seller = 0
     data.forEach(function(item) {
-      if (sellers.who_made === "i_did") {
-        seller++
+      if (seller.who_made === "i_did") {
+        seller ++
       }
     });
-    console.log(seller + "items made by seller");
-  
+    console.log(seller + "made by themselves");
+}
